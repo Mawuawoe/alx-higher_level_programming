efc3e104a00f00a71d20897145d633757d6e53e7
Mawuawoe <desmonddzakago@gmail.com>
2024-03-19 20:25:32 +0000

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,3 +2,8 @@
 if __name__ == "__main__":
     from hidden_4 import *
     all = dir()
+    for i in all:
+        if i[0] == '_' and i[1] == '_':
+            continue
+        else:
+            print(i)
