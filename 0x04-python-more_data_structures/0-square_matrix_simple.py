cea860fb70f8368e9b25a8d3f395ccd6b26d3834
Mawuawoe <desmonddzakago@gmail.com>
2024-03-25 21:19:24 +0000

    task0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrixA = []
+    for i in matrix:
+        squared_row = []
+        for element in i:
+            squared_row.append(element ** 2)
+        matrixA.append(squared_row)
+    return matrixA
+
+   # x = []
+   # for i in matrix:
+       # x.append(list(map(lambda x : x ** 2, i)))
+   # return(x)
