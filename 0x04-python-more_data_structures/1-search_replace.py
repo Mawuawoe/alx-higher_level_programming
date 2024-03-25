7f7525d60d4e7a746751d485adfaf8f2d88f85b8
Mawuawoe <desmonddzakago@gmail.com>
2024-03-25 22:08:29 +0000

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,7 @@
 def search_replace(my_list, search, replace):
     x = 0
     new_list = my_list[:]
-    for i in new_list:
-        if i == search:
-            break
-        else:
-            x = x + 1
-    new_list[x] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
