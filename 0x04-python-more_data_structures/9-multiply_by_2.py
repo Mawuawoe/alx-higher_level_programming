ea0cfdc692c56aa3bf06375d2b5dea91896fb4dd
Mawuawoe <desmonddzakago@gmail.com>
2024-03-26 13:28:11 +0000

    task9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     dict_to_multiply = dict(a_dictionary)
     for keys in dict_to_multiply:
-        dict_to_multiply.update({keys : (dict_to_multiply[keys] * 2)})
+        dict_to_multiply.update({keys: (dict_to_multiply[keys] * 2)})
     return dict_to_multiply
