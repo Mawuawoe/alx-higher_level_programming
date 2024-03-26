557c2359329ea1866e5eda4952cbdfa6bb3c1d37
Mawuawoe <desmonddzakago@gmail.com>
2024-03-26 10:37:38 +0000

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""def search_replace(my_list, search, replace):
-    x = 0
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list"""
-
+# def search_replace(my_list, search, replace):
+# x = 0
+# new_list = my_list[:]
+# for i in range(len(new_list)):
+# if new_list[i] == search:
+#      new_list[i] = replace
+# return new_list
 def search_replace(my_list, search, replace):
     def find_search(element):
         if element == search:
@@ -14,4 +13,4 @@ def search_replace(my_list, search, replace):
         else:
             return element
     new_list = list(map(find_search, my_list))
-    return(new_list)
+    return (new_list)
