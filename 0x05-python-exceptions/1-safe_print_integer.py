c09cf790d04b643c56108324d337b423ab79b27a
Mawuawoe <desmonddzakago@gmail.com>
2024-04-03 11:23:52 +0000

    task1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+def safe_print_integer(value):
+        try:
+            int_value = int(value)
+            print("{:d}".format(int_value))
+            return 1
+        except:
+            return 0
+
