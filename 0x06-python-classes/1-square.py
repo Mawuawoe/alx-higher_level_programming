ce8297dcbe04f70bfbcc925cfa8f442ea3eb1a8b
Mawuawoe <desmonddzakago@gmail.com>
2024-04-07 16:58:13 +0000

    task0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # 1-square.py by Des
-# Defines a square
+"""Defines a square"""
 class Square:
-    # Represents a square"""
+    """Represents a square"""
 
     def __init__(self, size):
-        # Initializing this square class
-        # Args: size - represnets the size of the square defined
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
         self.__size = size
