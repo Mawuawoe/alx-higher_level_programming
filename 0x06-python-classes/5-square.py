75bce66e471e237ce4e4828c9b246b60010d3beb
Mawuawoe <desmonddzakago@gmail.com>
2024-04-08 15:05:30 +0000

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# 1-square.py by Des
+"""Defines a square"""
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        Raise:
+        TypeError: if size is not interger
+        ValueError: if size is less than 0
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+ 
+    @property
+    def size(self):
+        """Retrieves size of square"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
