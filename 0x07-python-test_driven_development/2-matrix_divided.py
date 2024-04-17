27f27a0823a0ecf28d9358e59a88214d5633b965
Mawuawoe <desmonddzakago@gmail.com>
2024-04-17 17:10:36 +0000

    task2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+"""
+This module has 1 function that takes a matrix (list of lists) and divid by div
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+    this function divides all elements of a matrix by a given number
+    """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
     if not (isinstance(div, int) or isinstance(div, float)):
-        raise TypeError ("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     new_matrix = []
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
         row_lengths = []
         row_lengths.append(len(row))
         if len(set(row_lengths)) != 1:
@@ -17,7 +27,8 @@ def matrix_divided(matrix, div):
         result = []
         for i in row:
             if not (isinstance(i, int) or isinstance(i, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
             result.append(round(i / div, 2))
         new_matrix.append(result)
     return new_matrix
