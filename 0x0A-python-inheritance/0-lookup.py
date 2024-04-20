96b1541c6153726ffcef4f1f9bd71d8d80491fef
Mawuawoe <desmonddzakago@gmail.com>
2024-04-20 08:59:20 +0000

    task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+""" This module return a list of attribute
+and methods of an object
+"""
+
+
 def lookup(obj):
+    """ A function that return a list of attribute
+    and modules of an object
+    
+    Arg:
+        obj - the object
+    """
     return dir(obj)
