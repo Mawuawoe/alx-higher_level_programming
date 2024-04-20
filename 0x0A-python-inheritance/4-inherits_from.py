d49956dbac8a66e1a06ee04dab186236db42218c
Mawuawoe <desmonddzakago@gmail.com>
2024-04-20 13:00:17 +0000

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """
 This module has a function that returns True if the object
 is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+the specified class ; otherwise False.
 """
 
 
@@ -10,6 +10,6 @@ def inherits_from(obj, a_class):
     """
     function that returns True if the object is an instance of
     a class that inherited (directly or indirectly)
-    from the specified class ; otherwise False.
+    the specified class ; otherwise False.
     """
     return (issubclass(type(obj), a_class) and type(obj) is not a_class)
