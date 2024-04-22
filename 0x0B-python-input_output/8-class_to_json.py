d2ef2592a05cd85f6ab869e634826d75ba76b20a
Mawuawoe <desmonddzakago@gmail.com>
2024-04-22 16:06:31 +0000

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+this module return a dictionary that holds
+the attribute(variable) and their corresponding
+values of an object
+"""
+
+
 def class_to_json(obj):
+    """
+    the function
+    """
     return obj.__dict__
