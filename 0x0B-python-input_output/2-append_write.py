fee3e052b1ec28806741c3459caa9ccb34a0825e
Mawuawoe <desmonddzakago@gmail.com>
2024-04-22 07:01:46 +0000

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module append to file and return the
+number of characters written
+"""
+
+
 def append_write(filename="", text=""):
+    '''Appends a string to the end of a UTF8 text file
+    '''
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
