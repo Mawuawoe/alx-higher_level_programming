731553be635d73f576fd606965fa600693f5db09
Mawuawoe <desmonddzakago@gmail.com>
2024-04-22 15:03:43 +0000

    task7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-This module write to file and return the
-number of characters written
+This module has a function to write to a file
+it take any python obj via json 
 """
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """
-    function to write to a file
-    return:
-        number of characters written
+    Arg:
+        my_obj: it a python object and not a string.
+        so to write it to a .txt file we need to convert to
+        a string.
+        the json.dumps(my_obj) does this
+
+        filename: is the .txt file to write to 
     """
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(json.dumps(my_obj))
