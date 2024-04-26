af108f376b505264a8b1179922e8bb497619c28c
Mawuawoe <desmonddzakago@gmail.com>
2024-04-26 17:31:37 +0000

    task0

---
+++ 0x0C-python-almost_a_circle/tests/test_mytest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import unittest
+"""
+unittesting example
+"""
+
+
+class Test_example(unittest.TestCase):
+
+    def test_example(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main
