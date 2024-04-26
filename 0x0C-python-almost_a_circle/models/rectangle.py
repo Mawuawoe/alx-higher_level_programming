a89efd5c90000c839fc4635c732e180d43170686
Mawuawoe <desmonddzakago@gmail.com>
2024-04-26 22:43:47 +0000

    task5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,7 @@ class Rectangle(Base):
             if i < self.__height - 1:
                 rectangle += '\n'
         print(rectangle)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
