f17d33cf60f269f887a154c5eaaac436149c60a6
Mawuawoe <desmonddzakago@gmail.com>
2024-04-27 00:03:46 +0000

    task7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,22 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.__width = arg
+                elif a == 2:
+                    self.__height = arg
+                elif a == 3:
+                    self.__x = arg
+                elif a == 4:
+                    self.__y = arg
+                a += 1
