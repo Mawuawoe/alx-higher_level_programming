10572176eec1097744ea0098ee8218a6555fc607
Mawuawoe <desmonddzakago@gmail.com>
2024-05-01 16:47:50 +0000

    task_test

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
 
     r1 = Rectangle(10, 2)
     print(r1.id)
+    print(r1.width)
+    r1.width = 11
+    print(r1.width)
 
     r2 = Rectangle(2, 10)
     print(r2.id)
---
+++ 0x0C-python-almost_a_circle/models/test_base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+unittesting example
+"""
+import unittest
+from base import Base
+
+
+class Base_Test(unittest.TestCase):
+
+    def test_baseconstruct(self):
+        b1 = Base(12)
+        id_no = b1.id
+        self.assertEqual(id_no, 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
