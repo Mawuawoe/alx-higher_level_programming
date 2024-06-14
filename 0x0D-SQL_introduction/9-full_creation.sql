0529057ca893b890243fd4231e1f35ed46aca426
Mawuawoe <desmonddzakago@gmail.com>
2024-06-14 12:02:38 +0000

    task_9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- a script that creates a table called second_table in the current database in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- If the table first_table already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+
+INSERT INTO first_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
