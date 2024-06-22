f4a77e199e7c8fefe924572c591fc5fb2c06427f
Mawuawoe <desmonddzakago@gmail.com>
2024-06-22 14:18:53 +0000

    task_6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const toPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const toPrintLength = toPrint.length;
+
+for (let i = 0; i < toPrintLength; i++) {
+  console.log(toPrint[i]);
+}
