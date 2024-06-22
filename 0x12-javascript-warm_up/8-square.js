170355cf0781ffddd34c00f933a3383652961f0a
Mawuawoe <desmonddzakago@gmail.com>
2024-06-22 14:44:25 +0000

    task_8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const size = parseInt(args[2]);
+
+if (size) {
+  for (let x = 0; x < size; x++) {
+    console.log('x'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
