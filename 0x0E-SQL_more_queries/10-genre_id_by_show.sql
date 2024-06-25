b001ec36e7fa1cf86a5b4d09187df501d2b2cc97
Mawuawoe <desmonddzakago@gmail.com>
2024-06-25 20:24:08 +0000

    task_SQL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
---Import the database dump from hbtn_0d_tvshows to your MySQL server: download
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
 SELECT tv_shows.title, tv_show_genres.genre_id
