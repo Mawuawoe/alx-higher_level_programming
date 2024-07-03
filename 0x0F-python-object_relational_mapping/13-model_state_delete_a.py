a214ba5251aede63fe27c626c8825aac8753d9d8
Mawuawoe <desmonddzakago@gmail.com>
2024-07-03 16:44:09 +0000

    task_13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that updates the name of a State object in a database.
+It takes the user, password, and database name as command line arguments.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+from urllib.parse import quote_plus
+
+if __name__ == "__main__":
+    """
+    Connect to database and update state name
+    """
+    user = sys.argv[1]
+    # URL encode the password
+    password = quote_plus(sys.argv[2])
+    db = sys.argv[3]
+
+    db_url = f'mysql+mysqldb://{user}:{password}@localhost/{db}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Fetch all state containing the letter a
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    #loop to delete
+    for state in states:
+        session.delete(state)
+    session.commit()
+
+    session.close()
