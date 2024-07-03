10ffd589ca22ac878c6bf4ffd83348b043a55c9a
Mawuawoe <desmonddzakago@gmail.com>
2024-07-03 15:52:50 +0000

    task_11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+a script that list the content of a table in a database
+it take the user, passwd and database name as
+cmd line arg
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+from urllib.parse import quote_plus
+from sqlalchemy.orm import sessionmaker
+
+# Connect & Create tables
+if __name__ == "__main__":
+    """
+    connect to database
+    add new states
+    list all states in ASC order
+    """
+    user = sys.argv[1]
+    # URL encode the password
+    password = quote_plus(sys.argv[2])
+    db = sys.argv[3]
+
+    db_url = f'mysql+mysqldb://{user}:{password}@localhost/{db}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_1 = State(name='Louisiana')
+    session.add(state_1)
+    session.commit()
+
+    states = session.query(State)\
+        .order_by(State.id).all()
+
+    for state in states:
+        print(f'{state.id}: {state.name}')
+
+    session.close()
