3efcd57a208e1b4492a03ca09acf85abd7d05fbd
Mawuawoe <desmonddzakago@gmail.com>
2024-07-03 14:54:17 +0000

    task_8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+a script that list the content of a table in a database
+it take the user, passwd and database name as
+cmd line arg
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+from urllib.parse import quote_plus
+from sqlalchemy.orm import sessionmaker
+
+# Connect & Create tables
+if __name__ == "__main__":
+    """
+    connect to database
+    list all states in ASC order
+    """
+    user = sys.argv[1]
+    # URL encode the password
+    password = quote_plus(sys.argv[2])
+    db = sys.argv[3]
+
+    db_url = f'mysql+mysqldb://{user}:{password}@localhost/{db}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    if (state):
+        print(f'1: {state.name}')
+    else:
+        print("Nothing")
+
+    session.close()
