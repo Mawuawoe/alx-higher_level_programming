00833c768dc95e6748b2143388b0cb6bb844b88a
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 08:37:58 +0000

    task_4

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a POST request to the URL, and displays the body of the response
-# A variable email must be sent with the value test@gmail.com
-# A variable subject must be sent with the value "I will always be here for PLD"
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a POST request to the URL with the specified variables and display the body of the response
+# A Bash script that takes in a URL, sends a POST request to the URL, and displays the body of the response A variable email must be sent with the value test@gmail.com A variable subject must be sent with the value "I will always be here for PLD" Check if a URL was provided as an argument Send a POST request to the URL with the specified variables and display the body of the response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
