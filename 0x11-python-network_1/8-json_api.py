9d38b77c15420ca7961968b994041bcc21b4c88b
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 13:19:44 +0000

    task_8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
-
+"""
+Write a Python script that takes in
+a letter and sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 
 if __name__ == '__main__':
     from requests import post
