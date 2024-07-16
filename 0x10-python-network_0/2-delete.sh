5c411a4d35758a66e8f53eec9fe350833ff959e0
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 08:30:13 +0000

    task_curl

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-
-# Check if a URL was provided as an argument
-
-# Send a DELETE request to the URL and display the body of the response
+# A Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response Check if a URL was provided as an argument Send a DELETE request to the URL and display the body of the response
 curl -s -X DELETE "${1}"
