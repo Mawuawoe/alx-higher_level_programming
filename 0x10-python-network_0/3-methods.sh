88c6efd3691b64e9f4a59b9ffba85db3ab25685e
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 08:33:51 +0000

    task_3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL and displays all HTTP methods the server will accept
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send an OPTIONS request to the URL and display the Allow header
-curl -sI -X OPTIONS "$1" | grep "Allow" | cut -d ' ' -f2-
+# A Bash script that takes in a URL and displays all HTTP methods the server will accept Check if a URL was provided as an argument Send an OPTIONS request to the URL and display the Allow header
+curl -sI -X OPTIONS "${1}" | grep "Allow" | cut -d ' ' -f2-
