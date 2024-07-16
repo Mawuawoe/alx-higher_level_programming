4169f47600ed3c14fbae1fd7bc3c6fd00fe8d5f6
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 08:36:18 +0000

    task_4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A header variable X-School-User-Id must be sent with the value 98
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request to the URL with the custom header and display the body of the response
+# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response A header variable X-School-User-Id must be sent with the value 98 Check if a URL was provided as an argument Send a GET request to the URL with the custom header and display the body of the response
 curl -s -H "X-School-User-Id: 98" "$1"
