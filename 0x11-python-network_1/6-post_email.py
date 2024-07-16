18495efa9d3e2e8c2f1121e55ebee67d784625fd
Mawuawoe <desmonddzakago@gmail.com>
2024-07-16 13:14:49 +0000

    task_5

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response."""
-
+Write a Python script that takes in a URL
+and an email address,
+sends a POST request to the passed URL with
+the email as a parameter, and
+finally displays the body of the response.
+"""
 
 if __name__ == '__main__':
     from sys import argv
