3d4df865a81d8349d1ed3a5dd30447eee2125e47
Mawuawoe <desmonddzakago@gmail.com>
2024-08-09 13:28:45 +0000

    task_1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,12 +4,11 @@
 const fs = require('fs');
 // Get the file path from the first command-line argument
 const filePath = process.argv[2];
-const string_to_print = process.argv[3];
+const stringToprint = process.argv[3];
 
 // Read the file content in utf-8 encoding
-fs.writeFile(filePath, string_to_print, 'utf8', function (err) {
+fs.writeFile(filePath, stringToprint, 'utf8', function (err) {
   if (err) {
     console.error(err); // Print the error object if an error occurred
-    return;
   }
 });
