3ac550a954a1c5d7206895aca7df9a70aa3e2fa2
Mawuawoe <desmonddzakago@gmail.com>
2024-08-11 19:05:17 +0000

    task_4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,21 @@
 
 const request = require('request');
 const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     const data = JSON.parse(body);
-    const films = data.results;
-    let i = 0;
-
-    for (const film of films) { // Iterate over each film
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        i += 1; // Increment the counter when the character is found
-      }
-    }
-
-    console.log(i); // Output the count of films featuring the character
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
 });
